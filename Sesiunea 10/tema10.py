6f7f360e159db8bb9e09e4dbbac55a6068c4c3b0
BdotB <bogdan.belei1993@gmail.com>
2023-04-22 12:28:31 +0300

    test

---
+++ Sesiunea 10/tema10.py
@@ -0,0 +1,164 @@
+
+# OBLIGATORIU - grad de dificultate: Usor spre Mediu
+#
+#
+# 1. Implementează o clasă Login care să moștenească unittest.TestCase.
+#
+# In metoda de setUp():
+# - seteaza driver-ul (instanta de browser)
+# - acceseaza site-ul 'https://the-internet.herokuapp.com/'
+# - click pe Form Authentication
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Login(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://the-internet.herokuapp.com/')
+        self.driver.find_element(By.LINK_TEXT, 'Form Authentication').click()
+
+# In metoda de tearDown():
+# - quit la browser
+
+    def tearDown(self):
+        self.driver.quit()
+# ● Test 1
+# - Verifică dacă noul url e corect
+
+    def test_new_url_is_correct(self):
+        expected_url = 'https://the-internet.herokuapp.com/login'
+        self.assertEqual(self.driver.current_url, expected_url)
+
+# ● Test 2
+# - Verifică dacă page title e corect (page title este tag-ul title din <head>)
+    def test_page_title_is_correct(self):
+        expected_title = 'The Internet'
+        self.assertEqual(self.driver.title, expected_title)
+
+# ● Test 3
+# - Verifică daca textul de pe elementul xpath=//h2 e corect
+    def test_heading_text_is_correct(self):
+        expected_heading = 'Login Page'
+        heading = self.driver.find_element(By.XPATH, '//h2').text
+        self.assertEqual(heading, expected_heading)
+
+
+# ● Test 4
+# - Verifică dacă butonul de login este displayed
+    def test_login_button_is_displayed(self):
+        login_button = self.driver.find_element(By.CSS_SELECTOR, '#login button')
+        self.assertTrue(login_button.is_displayed())
+
+# ● Test 5
+# - Verifică dacă atributul href al linkului ‘Elemental Selenium’ e corect
+  def test_elemental_selenium_link_href_is_correct(self):
+        expected_href = 'http://elementalselenium.com/'
+        link = self.driver.find_element(By.LINK_TEXT, 'Elemental Selenium')
+        self.assertEqual(link.get_attribute('href'), expected_href)
+
+# ● Test 6
+# - Lasă goale user și pass
+# - Click login
+# - Verifică dacă eroarea e displayed
+  def test_empty_username_and_password_show_error(self):
+        self.driver.find_element(By.ID, 'username').send_keys('')
+        self.driver.find_element(By.ID, 'password').send_keys('')
+        self.driver.find_element(By.CSS_SELECTOR, '#login button').click()
+        error = self.driver.find_element(By.ID, 'flash').text
+        self.assertTrue('Your username is invalid!' in error)
+
+# ● Test 7
+# - Completează cu user și pass invalide
+# - Click login
+# - Verifică dacă mesajul de pe eroare e corect
+# - Este și un x pus acolo extra așa că vom folosi soluția de mai jos
+# expected = 'Your username is invalid!'
+# self.assertTrue(expected in actual, 'Error message text is
+# incorrect')
+    def test_invalid_username_and_password_show_error(self):
+        self.driver.find_element(By.ID, 'username').send_keys('invalid')
+        self.driver.find_element(By.ID, 'password').send_keys('invalid')
+        self.driver.find_element(By.CSS_SELECTOR, '#login button').click()
+        error = self.driver.find_element(By.ID, 'flash').text
+        expected_error = 'Your username is invalid!'
+        self.assertTrue(expected_error in error)
+# ● Test 8
+# - Lasă goale user și pass
+# - Click login
+# - Apasă x la eroare
+# - Verifică dacă eroarea a dispărut
+    def test_close_error_removes_error(self):
+        self.driver.find_element(By.ID, 'username').send_keys('')
+        self.driver.find_element(By.ID, 'password').send_keys('')
+        self.driver.find_element(By.CSS_SELECTOR, '#login button').click()
+        close = self.driver.find_element(By.CSS_SELECTOR, '#flash > .close')
+        close.click()
+        error_displayed = EC.visibility_of_element_located((By.ID, 'flash'))
+        self.assertFalse(error_displayed(self.driver))
+
+# ● Test 9
+# - Ia ca o listă toate //label
+# - Verifică textul ca textul de pe ele să fie cel așteptat (Username și
+# Password)
+# - Aici e ok să avem 2 asserturi
+  def test_labels_have_expected_text(self):
+        expected_labels = ['Username', 'Password']
+        labels = self.driver.find_elements(By.XPATH, '//label')
+        self.assertEqual(len(labels), len(expected_labels))
+        for i, label in enumerate(labels):
+            self.assertEqual(label.text, expected_labels[i])
+
+# ● Test 10
+# - Completează cu user și pass valide
+# - Click login
+# - Verifică ca noul url CONTINE /secure
+# - Folosește un explicit wait pentru elementul cu clasa ’flash succes’
+# - Verifică dacă elementul cu clasa=’flash succes’ este displayed
+#
+# - Verifică dacă mesajul de pe acest element CONȚINE textul ‘secure area!’
+
+    def test_login_success(self):
+        self.driver.find_element(By.ID, "username").send_keys("tomsmith")
+        self.driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+        self.driver.find_element(By.CSS_SELECTOR, ".fa-sign-in").click()
+        # Verifică ca noul url CONTINE /secure
+        self.assertIn('/secure', self.driver.current_url)
+        # Folosește un explicit wait pentru elementul cu clasa ’flash succes’
+        success_msg = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "flash.success")))
+        # Verifică dacă elementul cu clasa=’flash succes’ este displayed
+        self.assertTrue(success_msg.is_displayed())
+        # Verifică dacă mesajul de pe acest element CONȚINE textul ‘secure area!’
+        self.assertIn('secure area!', success_msg.text)
+
+# ● Test 11
+# - Completează cu user și pass valide
+# - Click login
+# - Click logout
+# - Verifică dacă ai ajuns pe https://the-internet.herokuapp.com/login
+    def test_logout(self):
+    self.driver.find_element(By.ID, "username").send_keys("tomsmith")
+    self.driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    self.driver.find_element(By.CSS_SELECTOR, ".fa-sign-in").click()
+    # Verifică ca noul url CONTINE /secure
+    self.assertIn('/secure', self.driver.current_url)
+    self.driver.find_element(By.CSS_SELECTOR, ".icon-2x.icon-signout").click()
+    # Verifică dacă ai ajuns pe https://the-internet.herokuapp.com/login
+    self.assertEqual(self.driver.current_url, "https://the-internet.herokuapp.com/login")
+
+# OPTIONAL - grad de dificultate: Mediu spre greu: may need Google
+# ● Test 12 - brute force password hacking
+# - Completează user tomsmith
+# - Găsește elementul //h4
+# - Ia textul de pe el și fă split după spațiu. Consideră fiecare cuvânt ca o
+# potențială parolă.
+# - Folosește o structură iterativă prin care să introduci rând pe rând
+# parolele și să apeși pe login.
+# - La final testul trebuie să îmi printeze fie
+# ‘Nu am reușit să găsesc parola’
+# ‘Parola secretă este [parola]’
+
---
+++ Sesiunea 9/tema9.py
@@ -0,0 +1,146 @@
+"""
+Tema 8 - SELECTORS
+"""
+
+"""
+Exerciții Recomandate - grad de dificultate: Ușor
+1. Revizualizează întâlnirea 7 și ia notițe în caz că ți-a scăpat ceva.
+Te rog să scrii pe canalul de comunicare scrisă unde întâmpini dificultăți și te
+ajut.
+Dacă stai blocat > 30 min, cere indiciu.
+"""
+
+"""
+Exerciții obligatorii - grad de dificultate: Usor spre Mediu
+Alege-ți unul sau mai multe din sugestiile de site-uri de mai jos
+- https://www.phptravels.net/
+- http://automationpractice.com/index.php
+- https://formy-project.herokuapp.com/
+- https://the-internet.herokuapp.com/
+- https://www.techlistic.com/p/selenium-practice-form.html
+- jules.app
+Alege câte 3 elemente din fiecare tip de selector din următoarele categorii:
+● Id
+● Link text
+● Parțial link text
+● Name
+● Tag*
+● Class name*
+● Css (1 după id, 1 după clasă, 1 după atribut=valoare_partiala)
+observație:
+- Probabil nu vei găsi un singur website care să cuprindă toate variantele
+de mai sus, astfel că îți recomandăm să folosești mai multe site-uri
+"""
+
+from selenium import webdriver
+
+# Deschide browser-ul
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.phptravels.net/")
+
+# Id
+search_form = driver.find_element_by_id("searchform")
+modal = driver.find_element_by_id("modal")
+header = driver.find_element_by_id("header")
+
+# Link text
+home = driver.find_element_by_link_text("Home")
+blog = driver.find_element_by_link_text("Blog")
+contact = driver.find_element_by_link_text("Contact")
+
+# Partial link text
+my_account = driver.find_element_by_partial_link_text("My Account")
+sign_up = driver.find_element_by_partial_link_text("Sign Up")
+book_now = driver.find_element_by_partial_link_text("Book Now")
+
+# Name
+email = driver.find_element_by_name("email")
+password = driver.find_element_by_name("password")
+country = driver.find_element_by_name("country")
+
+# Tag
+div = driver.find_element_by_tag_name("div")
+input = driver.find_element_by_tag_name("input")
+span = driver.find_element_by_tag_name("span")
+
+# Class name
+btn_primary = driver.find_element_by_class_name("btn btn-block btn-primary")
+btn_lg = driver.find_element_by_class_name("btn-lg")
+p1 = driver.find_element_by_class_name("p1")
+
+# CSS (1 dupa id, 1 dupa clasa, 1 dupa atribut=valoare_partiala)
+select2_drop = driver.find_element_by_css_selector("#select2-drop")
+navbar = driver.find_element_by_css_selector(".navbar")
+departure = driver.find_element_by_css_selector("[name^='departure']")
+
+# Inchide browser-ul
+driver.quit()
+'''
+"""
+- Opțional: La tag și class name vei folosi find elementS! - salvează în listă.
+Interactionează cu un element la alegere din listă.
+Pentru xpath identifică elemente după criteriile de mai jos:
+● 3 după atribut valoare
+● 3 după textul de pe element
+● 1 după parțial text
+● 1 cu SAU, folosind pipe |
+● 1 cu *
+● 1 în care le iei ca pe o listă de xpath și în python ajunge 1 element, deci
+cu (xpath)[1]
+● 1 în care să folosești parent::
+● 1 în care să folosești fratele anterior sau de după (la alegere)
+● 1 funcție ca și cea de la clasă prin care să pot alege eu prin parametru cu
+ce element vreau să interacționez.
+"""
+'''
+from selenium import webdriver
+
+# Deschide browser-ul
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.phptravels.net/")
+
+# 3 dupa atribut valoare
+elements_by_value = driver.find_elements_by_xpath("//input[@type='text' or @type='password' or @type='email']")
+
+# 3 dupa textul de pe element
+elements_by_text = driver.find_elements_by_xpath("//*[text()='Home' or text()='Blog' or text()='Contact']")
+
+# 1 dupa parțial text
+element_by_partial_text = driver.find_element_by_xpath("//*[contains(text(), 'Account')]")
+
+# 1 cu SAU, folosind pipe |
+element_by_pipe = driver.find_element_by_xpath("//input[@type='text' or @type='email']")
+
+# 1 cu *
+element_by_star = driver.find_element_by_xpath("//div/*")
+
+# 1 în care le iei ca pe o lista de xpath si în python ajunge 1 element, deci cu (xpath)[1]
+element_by_list = driver.find_element_by_xpath("(//input[@type='text' or @type='password' or @type='email'])[1]")
+
+# 1 în care sa folosesti parent::
+element_by_parent = driver.find_element_by_xpath("//input[@name='password']/parent::div")
+
+# 1 în care sa folosesti fratele anterior sau de dupa (la alegere)
+element_by_sibling = driver.find_element_by_xpath("//input[@name='email']/following-sibling::button")
+
+# 1 functie ca si cea de la clasa prin care sa pot alege eu prin parametru cu ce element vreau sa interactionez
+def interact_with_element(element_index):
+    elements = driver.find_elements_by_xpath("//a")
+    elements[element_index].click()
+
+# Interactioneaza cu al 2-lea element din lista de elemente "elements_by_text"
+elements_by_text[1].click()
+
+# Interactioneaza cu primul element din lista de elemente "elements_by_value"
+elements_by_value[0].send_keys("test@example.com")
+
+# Inchide browser-ul
+driver.quit()
+'''
+"""
+Exerciții extra - Opțional
+https://www.automatetheplanet.com/most-exhaustive-xpath-locators-cheat-sh
+eet/
+"""
